a7c0a839f5c3700fcf116c1fac9332277b5ee4a8
maxx <maxpetrikin@gmail.com>
2025-07-01 12:39:42 +0200

    language icons added

---
+++ mainapp/templatetags/lang_tags.py
@@ -0,0 +1,16 @@
+# mainapp/templatetags/lang_tags.py
+
+from django import template
+
+register = template.Library()
+
+LANG_ICONS = {
+    'python': 'fa-brands fa-python',
+    'cpp': 'fa-solid fa-code',
+    'java': 'fa-brands fa-java',
+    'js': 'fa-brands fa-js',
+}
+
+@register.simple_tag
+def lang_icon_class(lang):
+    return LANG_ICONS.get(lang.lower(), 'fa-solid fa-file-code')
