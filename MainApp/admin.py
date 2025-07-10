4cd80a42e5ca43f2a2e39630b8ce6eae1185abd9
maxx <maxpetrikin@gmail.com>
2025-07-10 11:44:39 +0200

    tag addition created

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,23 +1,49 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from MainApp.models import Snippet, Comment, Tag
+
 
 # Snippet
 @admin.register(Snippet)
 class SnippetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'lang', 'public', 'user')
-    list_filter = ('lang', 'public')
+    list_display = ('name', 'lang', 'public', 'user', 'creation_date', 'num_comments')
+    list_filter = ('lang', 'public', 'creation_date')
+    fields = ('name', 'lang', 'code', 'public', 'creation_date', 'user', 'tags')
+
     search_fields = ('name',)
-    fields = ('name', 'lang', 'code', 'public', 'user')
+
+# Доп.колонка кол-ва комментариев
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            num_comments=Count('comment', distinct=True)
+        )
+        return queryset
+
+    def num_comments(self, obj):
+        return obj.num_comments
+
+    num_comments.short_description = 'Кол-во комментариев'
+
 
 # Comment
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('text', 'author', 'snippet', 'creation_date')
+    list_display = ('text', 'author', 'snippet', 'creation_date', )
+    list_filter = ('author', 'creation_date')
+
     search_fields = ('text', 'author__username', 'snippet__name')
-    list_filter = ('author',)
+
 
 # Tag
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
     search_fields = ('name',)
+
+
+admin.site.site_header = "PythonBin - администрирование"
+admin.site.site_title = "PythonBin Admin"
+admin.site.index_title = "Добро пожаловать в администрирование PythonBin"
