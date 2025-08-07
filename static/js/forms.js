3fc00418432ac7e72849d542c8123abf10d946b6
maxx_PC <m.petrykin@gmx.de>
2025-08-07 10:31:51 +0200

    1

--- static/js/forms.js
+++ static/js/forms.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
           counter.classList.add('text-danger');
         }
 
+
         // Ограничение длины: если превышено, обрезаем
         if (length > maxLength) {
           codeField.value = codeField.value.slice(0, maxLength);
