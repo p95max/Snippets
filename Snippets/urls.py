639104e25f770375745cd49de729ffef401c8303
maxx_PC <m.petrykin@gmx.de>
2025-08-28 10:57:15 +0200

    media: отображение favicon исправлено

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -2,11 +2,14 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import RedirectView
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('MainApp.urls', namespace='MainApp'))
+    path('', include('MainApp.urls', namespace='MainApp')),
+
+    path('favicon.ico', RedirectView.as_view(url=static('media/favicon.svg'))),
 ] + debug_toolbar_urls()
 
 if settings.DEBUG:
