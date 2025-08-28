255bcb38037014075f8df2475825d57562efc770
maxx_PC <m.petrykin@gmx.de>
2025-08-28 11:11:58 +0200

    urls: исправлен импорт статики

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,9 +1,10 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
+from django.templatetags.static import static
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.views.generic import RedirectView
-from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +14,5 @@ urlpatterns = [
 ] + debug_toolbar_urls()
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static_serve(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static_serve(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
